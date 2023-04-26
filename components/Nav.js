9f3012e94cc4615d0c6c3e6966b5de32f06e7e5e
Tobi Faniran <tobifaniran@gmail.com>
2023-04-26 19:51:22 +0100

    font family

--- components/Nav.js
+++ components/Nav.js
@@ -3,17 +3,25 @@ import navIcon from "../public/assets/nav.svg";
 
 function Nav() {
   return (
-    <div className="relative ">
-      <div className="nav-items absolute flex justify-between w-full">
-        <div className="icon-img">
+    <div className="relative font-con">
+      <div className="nav-items absolute flex justify-between w-full py-10">
+        <div className="icon-img ml-10">
           <img src={navIcon.src} alt="nav icon" />
         </div>
-        <div className="nav-list">
-          <ul className="flex">
-            <li>00 Home</li>
-            <li>01 Destination</li>
-            <li>02 Crew</li>
-            <li>03 Technology</li>
+        <div className="nav-list  text-white">
+          <ul className="flex items-center bg-nav-color backdrop-blur-xs px-5 py-5 tracking-wide">
+            <li className="pr-7">
+              <span className="font-bold">00</span> HOME
+            </li>
+            <li className="pr-7">
+              <span className="font-bold">01</span> DESTINATION
+            </li>
+            <li className="pr-7">
+              <span className="font-bold">02</span> CREW
+            </li>
+            <li>
+              <span className="font-bold">03</span> TECHNOLOGY
+            </li>
           </ul>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,19 @@ module.exports = {
     extend: {
       backgroundImage: {
         "background-1": "url('../public/assets/Bg-1.png')",
+      },
+      colors: {
         "nav-color": "rgba(255, 255, 255, 0.04)",
+      },
+      letterSpacing: {
+        wide: "2.7px",
+      },
+      backdropBlur: {
+        xs: "40.7742px",
+      },
+      fontFamily: {
+        body: ["Barlow", "sans-serif"],
+        con: ["Barlow", "Condensed"],
       },
     },
   },
