5292b32b1b94b155138a2c608ec57dd74d9386b7
Tobi Faniran <tobifaniran@gmail.com>
2023-04-28 22:13:30 +0100

    space dummy data

--- pages/crew.tsx
+++ pages/crew.tsx
@@ -3,7 +3,13 @@ import React from "react";
 function Crew() {
   return (
     <div className="bg-background-3 bg-cover bg-no-repeat min-h-screen flex place-items-center w-full">
-      Crew
+      <div className="crew-container ">
+        <div className="sub-header font-con ml-[10rem]">
+          <p className="text-white tracking-[4.725px] text-[20px]">
+            <span className="text-white opacity-25">02</span> MEET YOUR CREW
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
--- utils/sample-data.ts
+++ utils/sample-data.ts
@@ -6,6 +6,12 @@ interface SpaceData {
   distance: string;
   time: string;
 }
+interface CrewData {
+  occupation: string;
+  image: string;
+  name: string;
+  description: string;
+}
 
 /** Dummy space data. */
 export const sampleUserData: SpaceData[] = [
@@ -44,5 +50,36 @@ export const sampleUserData: SpaceData[] = [
       "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
     distance: "1.6 BIL. km",
     time: "7 years",
+  },
+];
+
+export const crewData: CrewData[] = [
+  {
+    occupation: "PILOT",
+    name: "Victor Glover",
+    description:
+      "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. ",
+    image: "/assets/crew-2.png",
+  },
+  {
+    occupation: "Flight Engineer",
+    name: "Anousheh Ansari",
+    description:
+      "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.  ",
+    image: "/assets/crew-3.png",
+  },
+  {
+    occupation: "Mission Specialist ",
+    name: "MARK SHUTTLEWORTH",
+    description:
+      "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.",
+    image: "/assets/crew-1.png",
+  },
+  {
+    occupation: "Commander ",
+    name: "Douglas Hurley",
+    description:
+      "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.",
+    image: "/assets/crew-4.png",
   },
 ];
