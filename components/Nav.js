a5e1981d472ff6a1b4350bfe300cfdca69c8e1c9
Tobi Faniran <tobifaniran@gmail.com>
2023-04-28 17:23:55 +0100

    active nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,12 @@
 import React from "react";
 import navIcon from "../public/assets/nav.svg";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Nav() {
+  const router = useRouter();
+  const { pathname } = router;
+  console.log(pathname);
   return (
     <div className="relative font-con">
       <div className="nav-items absolute flex justify-between items-center w-full py-10">
@@ -11,24 +15,40 @@ function Nav() {
         </div>
         <div className="line absolute left-[170px] top-18 mix-blend-normal w-[400px] h-px opacity-25 bg-white z-50"></div>
         <div className="nav-list w-3/5 text-white md:hidden">
-          <ul className="flex items-center bg-nav-color backdrop-blur-xs px-5 py-5 tracking-wide justify-center">
+          <ul className="flex items-center  bg-nav-color backdrop-blur-xs px-5 py-5 tracking-wide justify-center">
             <Link href="/">
-              <li className="pr-10 ">
+              <li
+                className={`pr-10 relative ${
+                  pathname === "/" ? "nav-active" : ""
+                }`}
+              >
                 <span className="font-bold">00</span> HOME
               </li>
             </Link>
             <Link href="/destination">
-              <li className="pr-10">
+              <li
+                className={`pr-10 relative ${
+                  pathname === "/destination" ? "nav-active" : ""
+                }`}
+              >
                 <span className="font-bold">01</span> DESTINATION
               </li>
             </Link>
             <Link href="/crew">
-              <li className="pr-10">
+              <li
+                className={`pr-10 relative ${
+                  pathname === "/crew" ? "nav-active" : ""
+                }`}
+              >
                 <span className="font-bold">02</span> CREW
               </li>
             </Link>
             <Link href="/technology">
-              <li>
+              <li
+                className={`relative ${
+                  pathname === "/technology" ? "nav-active" : ""
+                }`}
+              >
                 <span className="font-bold">03</span> TECHNOLOGY
               </li>
             </Link>
